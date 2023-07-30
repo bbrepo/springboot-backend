2ec1a46b4ec23ac1439aa0da0156d013d5933098
bidhan_gl <bidhanbiswas.cse@gmail.com>
2023-07-29 23:53:26 -0500

    Initial commit

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
@@ -0,0 +1,31 @@
+package net.javaguides.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+	
+	private static final long serialVersionUID=1L;
+	private String resourceName;
+	private String fieldName;
+	private Object fieldValue;
+	
+	public ResourceNotFoundException(String resourceName, String fieldname, Object fieldValue) {
+	   super(String.format("%s not found with %s: '%s'", resourceName,fieldname,fieldValue));
+	   this.resourceName=resourceName;
+	   this.fieldName=fieldName;
+	   this.fieldValue=fieldValue;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+	public String getFieldName() {
+		return fieldName;
+	}
+	public Object getFieldValue() {
+		return fieldValue;
+	}
+	
+}
